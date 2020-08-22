5a377408e996cb037a3e3429a42d25e29dfbe249
Levi <27644153+levitannin@users.noreply.github.com>
2020-08-22 14:42:04 -0400

    Create csj_ch2_challenge.py

---
+++ codeforsocialjustice/csj_ch2_challenge.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Aug 22 14:33:53 2020
+
+Following the "Coding for Social Justice" challenges by Eric Matthes.
+Challenges can be found here:
+    https://ehmatthes.github.io/pcc_2e/challenges/coding_for_social_justice/
+
+Chapter 2 Challenge:
+    Print the message you would put on a sign for a march or protest.
+
+@author: Levitannin
+"""
+
+message = "Being late to Listen, but that isn't a Reason to Ignore Cries for Help or Demands for Justice"
+print(message)
